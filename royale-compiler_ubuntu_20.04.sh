f26434120be90f782a7912e00627dcdceb212e14
santosh653 <70637961+santosh653@users.noreply.github.com>
2021-04-16 13:18:13 +0530

    Update royale-compiler_ubuntu_20.04.sh
    Adding new build script.

--- royale-compiler_ubuntu_20.04.sh
+++ royale-compiler_ubuntu_20.04.sh
@@ -1,10 +1,13 @@
-# Version	    : 0.1.0 
-# Source repo	: https://github.com/apache/royale-compiler
-# Tested on	    : ubuntu_20.04
-# Script License: Apache License, Version 2 or later
-# Maintainer	: Santosh Kulkarni <santoshkulkarni70@gmail.com> / Priya Seth<sethp@us.ibm.com>
+# ----------------------------------------------------------------------------
 #
-# Disclaimer: This script has been tested in non-root mode on given
+# Package           : Royale-compiler
+# Version           : 0.1.0
+# Source repo       : https://github.com/apache/royale-compiler
+# Tested on         : ubuntu_20.04
+# Script License    : Apache License, Version 2 or later
+# Maintainer        : Santosh Kulkarni <santoshkulkarni70@gmail.com> / Priya Seth<sethp@us.ibm.com>
+#
+# Disclaimer        : This script has been tested in non-root mode on given
 # ==========  platform using the mentioned version of the package.
 #             It may not work as expected with newer versions of the
 #             package and/or distribution. In such case, please
@@ -13,24 +16,38 @@
 # ----------------------------------------------------------------------------
 #!/bin/bash
 
+
+export REPO=https://github.com/apache/royale-compiler
+
+if [ -z "$1" ]; then
+  export VERSION="org.apache.royale.compiler-0.9.8-rc2"
+else
+  export VERSION="$1"
+fi
+
 sudo apt-get update
 sudo apt-get install openjdk-8-jdk wget git -y
 export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-ppc64el/
-
-wget http://www-eu.apache.org/dist/maven/maven-3/3.5.2/binaries/apache-maven-3.5.2-bin.tar.gz
-tar xzvf apache-maven-3.5.2-bin.tar.gz
-export PATH=$PATH:`pwd`/apache-maven-3.5.2/bin
 sudo apt install  -y maven
 mvn -version
-git clone https://github.com/eclipse-ee4j/jersey
-## Build and test jersey
-cd jersey
+
+if [ -d "royale-compiler" ] ; then
+  rm -rf royale-compiler
+fi
+
+git clone ${REPO}
+
+## Build and test JTransforms
+cd royale-compiler
+git checkout ${VERSION}
+ret=$?
+
+if [ $ret -eq 0 ] ; then
+  echo "$Version found to checkout "
+else
+  echo "$Version not found "
+  exit
+fi
+
 mvn clean install
 
-## Note ##
-# Running build Without Tests
-# mvn clean install -DskipTests=true
-# Then Running Tests saperately 
-# mvn test
-# Running build and test in same command 
-# mvn clean install
